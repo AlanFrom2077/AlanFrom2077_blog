5e383a47faab663e42726f99e41ae64e0fd280b7
2077deAyst <147007159+2077deAyst@users.noreply.github.com>
2026-04-23 18:18:24 -0400

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,42 @@
 </script>
 
 <template>
-  
+  <div class="h-screen bg-cover bg-center bg-[url(src/assets/homeBg/1.jpg)]">
+    <div class="h-screen bg-cover bg-center">
+
+      <div class="bg-gray-300 w-screen h-11 fixed opacity-50">
+        <div class="float-left w-1/5 pt-2.5">
+          <center>
+            <p>AlanFrom2077的小站</p>
+          </center>
+        </div>
+
+        <div class="center grid grid-cols-4 gap4 float-left w-4/5">
+          <div class="place-content-center h-11">
+            <center>
+              <p class="text-black hover:text-white">主页</p>
+            </center>
+          </div>
+          <div class="place-content-center h-11">
+            <center>
+              <p class="text-balck hover:text-white">项目</p>
+            </center>
+          </div>
+          <div class="place-content-center h-11">
+            <center>
+              <p class="text-black hover:text-white">博客</p>
+            </center>
+          </div>
+          <div class="place-content-center h-11">
+            <center>
+              <p class="text-black hover:text-white">资源</p>
+            </center>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
 </template>
